05d1f08c2ef6217f3e4c4dd06e8bb2a359d5bae9
Fabiu <131700736+fab24742@users.noreply.github.com>
2024-11-07 23:09:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('comprar').addEventListener('click', () => {
+    const cantidad = document.getElementById('cantidad').value;
+    let mensaje = `Vengo de la web, deseo comprar ${cantidad} pack${cantidad > 1 ? 's' : ''} de COFI.`;
+    window.open(`https://wa.me/tu-numero?text=${encodeURIComponent(mensaje)}`, '_blank');
+});
+
+document.getElementById('informacion').addEventListener('click', () => {
+    let mensaje = "Vengo de la web, deseo más información sobre COFI.";
+    window.open(`https://wa.me/tu-numero?text=${encodeURIComponent(mensaje)}`, '_blank');
+});
+
