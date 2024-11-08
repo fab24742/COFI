dad9f01de56719d43761c7f1ae9d1cf4bde3227f
Fabiu <131700736+fab24742@users.noreply.github.com>
2024-11-07 23:39:09 -0500

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.getElementById('comprar').addEventListener('click', () => {
     const cantidad = document.getElementById('cantidad').value;
     let mensaje = `Vengo de la web, deseo comprar ${cantidad} pack${cantidad > 1 ? 's' : ''} de COFI.`;
-    window.open(`https://wa.me/925773294?text=${encodeURIComponent(mensaje)}`, '_blank');
+    window.open(`https://wa.me/51925773294?text=${encodeURIComponent(mensaje)}`, '_blank');
 });
 
 document.getElementById('informacion').addEventListener('click', () => {
     let mensaje = "Vengo de la web, deseo más información sobre COFI.";
-    window.open(`https://wa.me/925773294text=${encodeURIComponent(mensaje)}`, '_blank');
+    window.open(`https://wa.me/51925773294?text=${encodeURIComponent(mensaje)}`, '_blank');
 });
-
