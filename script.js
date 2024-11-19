15bfbc6632828ab9514cfb6a12ae84f8cc64831d
Fabiu <131700736+fab24742@users.noreply.github.com>
2024-11-19 01:39:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.querySelectorAll('.primary-button').forEach(button => {
     button.addEventListener('click', () => {
         const pack = button.getAttribute('data-pack');
         const precio = button.getAttribute('data-precio');
-        const mensaje = `Hola! Quiero comprar ${pack} COFI Pack(s) por S/${precio}. Por favor, contáctame para más detalles.`;
-        window.open(`https://wa.me/51925773294?text=${encodeURIComponent(mensaje)}`, '_blank');
+        const mensaje = `Hola! Quiero comprar ${pack} COFI Pack(s) por S/${precio}.`;
+        window.open(`https://wa.me/51951182402?text=${encodeURIComponent(mensaje)}`, '_blank');
     });
 });
 
